722da1533d701622c1fd6457364c6dca8db0b6d0
HeilTchambaNana <htchamba124@gmail.com>
2026-04-12 19:35:05 +0100

    Initialisation de l'api gateway

---
+++ src/main/java/com/dreamhouse237/proxy_service/ProxyServiceApplication.java
@@ -0,0 +1,14 @@
+package com.dreamhouse237.proxy_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServiceApplication.class, args);
+	}
+
+}
